2ec0053650b71c2e17af24eb0b13f4ad0ba4c8bf
hdhdhasr-ctrl <hdhdhasr@gmail.com>
2026-07-09 16:29:23 +0200

    Update send-booking.js

--- frontend/netlify/functions/send-booking.js
+++ frontend/netlify/functions/send-booking.js
@@ -153,21 +153,9 @@ exports.handler = async function (event, context) {
         smsParams.append('to', smsplanetPhone);
         smsParams.append('msg', `Nowa rezerwacja: ${name}, ${service}, ${date} ${time}, tel: ${phone}`);
 
-        const smsResponseText = await smsResponse.text();
-      console.log('SMSPlanet raw response:', smsResponseText);
-
-      let smsData = {};
-      try {
-      smsData = JSON.parse(smsResponseText);
-      } catch (e) {
-      smsData = { rawText: smsResponseText };
-      }
-
-      if (smsResponse.ok && smsData.id) {
-      smsSuccess = true;
-      } else {
-      smsErrorMsg = `SMSPlanet response: ${smsResponseText}`;
-      }
+        const smsResponse = await fetch(`https://api2.smsplanet.pl/sms?${smsParams.toString()}`, {
+        method: 'GET'
+        });
 
         if (smsResponse.ok) {
           const smsData = await smsResponse.json().catch(() => ({}));
